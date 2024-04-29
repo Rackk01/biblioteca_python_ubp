8b80814aa120bf5475b920330d84695b7df7a588
__Facud462__ <facud462@gmail.com>
2024-04-29 18:51:23 -0300

    cambios en database v2.0

--- DB/sitio.sql
+++ DB/sitio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-04-2024 a las 03:05:09
+-- Tiempo de generación: 29-04-2024 a las 23:49:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `descargas`
+--
+
+CREATE TABLE `descargas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `libro_id` int(11) NOT NULL,
+  `fecha_descarga` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `libros`
 --
 
@@ -39,11 +52,45 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id`, `nombre`, `imagen`, `url`) VALUES
-(6, 'el libro de la selva', '2024214114_el libro de la selva.jpg', 'http://conectateamiclase.com/wp-content/uploads/2020/05/EL-REY-LEON.pdf');
+(1, 'El libro gordo de petete', '2024004128_Petete.jpg', 'https://github.com/Rackk01/biblioteca_python_ubp'),
+(2, 'El libro de la selva', '2024004150_el libro de la selva.jpg', 'https://github.com/Rackk01/biblioteca_python_ubp'),
+(3, 'Rey Leon', '2024004211_portada_el-rey-leon-la-novela_disney_201907112105.jpg', 'https://github.com/Rackk01/biblioteca_python_ubp'),
+(4, '100 años de soledad ', '2024153551_7.-Cien-años-Max-Rompo-196x300.jpg', 'https://github.com/');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre_usuario` varchar(255) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `rol` enum('usuario','administrador') NOT NULL DEFAULT 'usuario'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre_usuario`, `nombre`, `apellido`, `contraseña`, `rol`) VALUES
+(1, 'facu', 'gabriel', 'diaz', '$2b$12$89YeRhXRm/vJQ7T5.VgvS.KvdV6HCTV3vYL/xDFv0E8aPJDiTEFQq', 'administrador'),
+(3, 'fredo', 'alfredo', 'bertoglio', '$2b$12$dc.FfFv1/I.jvgZjo0JLP.NBFCZogOrxkSYAelsfBuZtVgHNWR0AC', 'usuario');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `descargas`
+--
+ALTER TABLE `descargas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `libro_id` (`libro_id`);
 
 --
 -- Indices de la tabla `libros`
@@ -52,14 +99,43 @@ ALTER TABLE `libros`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `descargas`
+--
+ALTER TABLE `descargas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `descargas`
+--
+ALTER TABLE `descargas`
+  ADD CONSTRAINT `descargas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `descargas_ibfk_2` FOREIGN KEY (`libro_id`) REFERENCES `libros` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
